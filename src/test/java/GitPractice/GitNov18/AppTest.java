439ec9ea10e9a99789360eeb7e5c3ad73b8df4ae
adeeb raheja <raheja.adeeb@gmail.com>
2025-11-18 22:31:40 -0500

    first commit

---
+++ src/test/java/GitPractice/GitNov18/AppTest.java
@@ -0,0 +1,23 @@
+package GitPractice.GitNov18;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void methodOne() {
+       System.out.println("hello");
+    }
+    
+    
+    
+    
+    
+    
+    
+}
